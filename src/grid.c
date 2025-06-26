26ba3fd8c53db409963f6d9ed0d635e16fc3c620
tadeyemo32 <tadeyemo32@gmail.com>
2025-06-27 00:44:16 +0800

    add walls

--- src/grid.c
+++ src/grid.c
@@ -10,7 +10,7 @@ int gridSize = 800/GRID_ROWS;
 
 pathType grid[GRID_ROWS][GRID_COLS];
 
-
+pathType curr ; 
 void init_grid(){
     for (int i = 0;i<GRID_ROWS;i++){
         for(int j =0;j<GRID_COLS;j++){
@@ -29,11 +29,32 @@ void draw_grid(){
             DrawLine(i * gridSize, 0, i * gridSize, SCREEN_H, RAYWHITE);
 
         for(int j = 0;j<GRID_COLS;j++){
-            DrawLine(0, j * gridSize, SCREEN_W, j * gridSize, RAYWHITE);
-            DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,SKYBLUE);
-           
+         DrawLine(0, j * gridSize, SCREEN_W, j * gridSize, RAYWHITE);
+int row = i ; 
+int col = j; 
+curr = grid[i][j];
+         if(row==0){
+            grid[i][j] = CELL_WALL;
+         }else if (row == GRID_ROWS-1){
+            grid[i][j] = CELL_WALL;
+         }else if(col== 0){
+        grid[i][j] = CELL_WALL;
+         }else if (col ==GRID_COLS-1){
+        grid[i][j] = CELL_WALL;
+         }
 
-
+    if (curr == CELL_EMPTY) {
+ DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,BROWN);} 
+ else if (curr == CELL_WALL) {
+ DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,LIGHTGRAY);} 
+ else if (curr == CELL_START) {
+ DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,GREEN);
+} else if (curr == CELL_GOAL) {
+ DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,RED);
+} else if (curr == CELL_PATH) {
+ DrawRectangle(i * gridSize, j*gridSize,gridSize,gridSize,BLUE);
+} 
+    
         }
     }
     
